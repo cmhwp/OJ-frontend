21913d5f4deaa1bd8367f488246305ef3fa1abd0
cmhwp <2232706035@qq.com>
2024-03-02 16:37:41 +0800

    Arco Design 框架自动导入集成

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite';
+import { ArcoResolver } from 'unplugin-vue-components/resolvers';
+import { vitePluginForArco } from '@arco-plugins/vite-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ArcoResolver()],
+    }),
+    Components({
+      resolvers: [
+        ArcoResolver({
+          sideEffect: true
+        })
+      ]
+    }),
+    vitePluginForArco({
+      style: 'css'
+    })
   ],
   resolve: {
     alias: {
